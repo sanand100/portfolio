bd24cf96133be7bb0879c89c4ebceb51bf07abb1
sanand100 <shraddhaanand1@gmail.com>
2022-05-25 14:22:22 -0400

    complete modal show and modal close css styling//complete about me modal

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('hello from js');
+
+//saving all the querySelectors here
+const aboutMeBtn = document.querySelector('#about-me');
+
+const aboutMeSection = document.querySelector('.about-me');
+
+const abtMeCloseBtn = document.querySelector('.close-md-abtme');
+//add event listener
+//creating event listeners for each of the navs
+aboutMeBtn.addEventListener('click', function () {
+	aboutMeSection.classList.add('modal-show');
+});
+
+abtMeCloseBtn.addEventListener('click', function () {
+	aboutMeSection.classList.remove('modal-show');
+});
