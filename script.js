49d82ca24c404a97f8227df9b0d84599e5d00b30
sanand100 <shraddhaanand1@gmail.com>
2022-05-25 21:29:37 -0400

    finish contact me page v1, add javascript for modals

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 console.log('hello from js');
 
 //saving all the querySelectors here
+const homePage = document.querySelector('.header-nav-container');
+
 const aboutMeBtn = document.querySelector('#about-me');
-
-const aboutMeSection = document.querySelector('.about-me');
-
+const aboutMeSection = document.querySelector('.about-me-container');
 const abtMeCloseBtn = document.querySelector('.close-md-abtme');
+
+const contactMeBtn = document.querySelector('#contact-me');
+const contactMeSection = document.querySelector('.contact-me-container');
+const contactMeCloseBtn = document.querySelector('.close-md-contactme');
+
+const projectsBtn = document.querySelector('#my-projects');
+const projectSection = document.querySelector('.my-projects-container');
+const projectsCloseBtn = document.querySelector('.close-md-projects');
+
 //add event listener
 //creating event listeners for each of the navs
 aboutMeBtn.addEventListener('click', function () {
 	aboutMeSection.classList.add('modal-show');
+	homePage.classList.add('modal');
 });
-
 abtMeCloseBtn.addEventListener('click', function () {
 	aboutMeSection.classList.remove('modal-show');
+	homePage.classList.remove('modal');
+});
+
+contactMeBtn.addEventListener('click', function () {
+	contactMeSection.classList.add('modal-show');
+	homePage.classList.add('modal');
+});
+contactMeCloseBtn.addEventListener('click', function () {
+	contactMeSection.classList.remove('modal-show');
+	homePage.classList.remove('modal');
+});
+
+projectsBtn.addEventListener('click', function () {
+	projectSection.classList.add('modal-show');
+	homePage.classList.add('modal');
+});
+projectsCloseBtn.addEventListener('click', function () {
+	projectSection.classList.remove('modal-show');
+	homePage.classList.remove('modal');
 });
